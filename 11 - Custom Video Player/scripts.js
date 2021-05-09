0f4b9ed7c275517cd9aedc96a98f8dc1c3d28cbd
Kyle S. Johnson <kyle1081@gmail.com>
2021-05-08 22:35:40 -0700

    Added fullscreen to video player.

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,9 +1,11 @@
-const video=document.querySelector('.player__video');
-const progress=document.querySelector('.progress');
-const progressIndicator=document.querySelector('.progress__filled');
-const playPauseButton=document.querySelector('.player__button.toggle');
-const skipButtons=document.querySelectorAll('.player__button[data-skip]');
-const sliders=document.querySelectorAll('.player__slider');
+const player=document.querySelector('.player');
+const video=player.querySelector('.player__video');
+const progress=player.querySelector('.progress');
+const progressIndicator=player.querySelector('.progress__filled');
+const playPauseButton=player.querySelector('.player__button.toggle');
+const skipButtons=player.querySelectorAll('.player__button[data-skip]');
+const sliders=player.querySelectorAll('.player__slider');
+const fullscreenButton=player.querySelector('.player__button.fullscreen');
 
 video.addEventListener("click",playPause);
 video.addEventListener("play",updatePlayPauseButton);
@@ -20,6 +22,18 @@ progress.addEventListener('mouseup', () => isSeeking = false);
 
 //window.setInterval(updateProgressBar,1000);
 video.addEventListener("timeupdate",updateProgressBar);
+
+
+fullscreenButton.addEventListener('click',toggleFullscreen);
+// document.addEventListener("keydown", (event)=>{
+// 	if(event.key==="Escape"){
+// 		toggleFullscreen();
+// 	}
+// });
+document.addEventListener('fullscreenchange',updateFullscreenButton);
+
+
+
 
 function updatePlayPauseButton(){
 	playPauseButton.textContent=(video.paused)?'►' : '❚ ❚';
@@ -50,4 +64,18 @@ function sliderChanged() {
 
 function seek(event) {
 	video.currentTime=video.duration*(event.offsetX/progress.offsetWidth);
+}
+
+
+function updateFullscreenButton(){
+	fullscreenButton.textContent=(document.fullscreenElement)?'> <' : '[ ]';
+}
+
+function toggleFullscreen() {
+	if(document.fullscreenElement){
+		document.exitFullscreen();
+	}
+	else {
+		player.requestFullscreen();
+	}
 }
